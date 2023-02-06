74675e91711d669c12029fc48bc7b2ad08ac0d97
Ryan Rubin <ryan.rubin@ph.softvision.global>
2023-02-06 17:24:34 +0800

    Play or pause

--- MauiSeekBar/MainPage.xaml.cs
+++ MauiSeekBar/MainPage.xaml.cs
@@ -5,5 +5,15 @@ public partial class MainPage : ContentPage
     public MainPage()
     {
         InitializeComponent();
+        NavigatedTo += MainPage_NavigatedTo;
+    }
+
+    private void MainPage_NavigatedTo(object? sender, NavigatedToEventArgs e)
+    {
+        var vm = new MainViewModel();
+        vm.Play += () => media.Play();
+        vm.Pause += () => media.Pause();
+        vm.GetCurrentState += () => media.CurrentState;
+        BindingContext = vm;
     }
 }
---
+++ MauiSeekBar/MainViewModel.cs
@@ -0,0 +1,33 @@
+﻿using CommunityToolkit.Maui.Core.Primitives;
+using System.ComponentModel;
+using System.Windows.Input;
+
+namespace MauiSeekBar;
+
+public class MainViewModel : INotifyPropertyChanged
+{
+    public Action? Play { get; set; }
+    public Action? Pause { get; set; }
+    public Func<MediaElementState>? GetCurrentState { get; set; }
+
+    public ICommand? PlayOrPauseCommand { get; set; }
+
+    public MainViewModel()
+    {
+        PlayOrPauseCommand = new Command(PlayOrPause);
+    }
+
+    private void PlayOrPause()
+    {
+        if (GetCurrentState?.Invoke() == MediaElementState.Playing)
+        {
+            Pause?.Invoke();
+        }
+        else
+        {
+            Play?.Invoke();
+        }
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+}
